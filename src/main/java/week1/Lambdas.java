9b2cb8bf9356709535cfcfcb6219bb15a67f3fa6
Lee Raulin <leeraulin@gmail.com>
2019-12-12 20:43:31 -0500

    ...

--- src/main/java/week1/Lambdas.java
+++ src/main/java/week1/Lambdas.java
@@ -29,5 +29,39 @@ public class Lambdas {
         items.sort((String item1, String item2) -> item1.substring(0, 1).compareTo(item2.substring(0, 1)));
         System.out.println(items);
 
+        System.out.println("Strings beginning with 'e' first, everything else next");
+        items.sort((String item1, String item2) -> {
+            if (item1.substring(0, 1).equalsIgnoreCase("e") && !item2.substring(0, 1).equalsIgnoreCase("e")) {
+                return -1;
+            } else if (!item1.substring(0, 1).equalsIgnoreCase("e") && item2.substring(0, 1).equalsIgnoreCase("e")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        System.out.println(items);
+        System.out.println("Shortest to longest");
+        items.sort((String item1, String item2) -> item1.length() - item2.length());
+        System.out.println(items);
+
+        System.out.println("Longest to shortest");
+        items.sort((String item1, String item2) -> item2.length() - item1.length());
+        System.out.println(items);
+
+        System.out.println("Alphabetically by first letter only");
+        items.sort((String item1, String item2) -> item1.substring(0, 1).compareTo(item2.substring(0, 1)));
+        System.out.println(items);
+
+        System.out.println("Strings beginning with 'e' first, everything else next");
+        items.sort((String item1, String item2) -> {
+            if (item1.substring(0, 1).equalsIgnoreCase("e") && !item2.substring(0, 1).equalsIgnoreCase("e")) {
+                return -1;
+            } else if (!item1.substring(0, 1).equalsIgnoreCase("e") && item2.substring(0, 1).equalsIgnoreCase("e")) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        System.out.println(items);
     }
 }
